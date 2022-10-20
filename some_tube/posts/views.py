80c23b654cda7c4baae9f814bf5136837417bf51
Vladyslav Svyrydenko <svyrydenko.vladyslav@gmail.com>
2022-10-20 09:27:20 +0300

    add new models, optimization queries

--- some_tube/posts/views.py
+++ some_tube/posts/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
-    latest = list(Post.objects.order_by("-pub_date")[:10])
+    latest = list(Post
+                  .objects
+                  .order_by("-pub_date")[:10]
+                  .select_related("author"))
     return render(request, "index.html", {"posts": latest})
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all().order_by("-pub_date")[:10]
+    context = {"group": group, "posts": posts}
+    return render(request, "group.html", context)
